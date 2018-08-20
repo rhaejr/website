b52e179e1494923de4ad2c4de9eb1a880a54c4cd
computer <av8r.08@gmail.com>
2018-08-20 14:07:19 -0500

    included SB Admin template pages. Need to create views for different pages and fix bugs related to template not being designed for django

--- website/landingpage/urls.py
+++ website/landingpage/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [ path('', views.index, name='index')]
+urlpatterns = [ 
+    path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    
+    ]
--- website/landingpage/views.py
+++ website/landingpage/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def index(request):
-    return render(request, 'landingpage/home.html')
+    return HttpResponse(render(request,'landingpage/home.html'))
+
+def login(request):
+    return render(request, 'landingpage/login.html')
