9e10769603f372931f16f6f7e757f11531998e32
computer <av8r.08@gmail.com>
2018-08-21 16:23:29 -0500

    logging out doesn't work yet

--- website/landingpage/urls.py
+++ website/landingpage/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
+import django.contrib.auth.views as auth_views
 from . import views
 
 urlpatterns = [ 
     path('', views.index, name='index'),
     path('index/', views.index, name='index'),
-    path('login/', views.login, name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('register/', views.register, name='register'),
+    path('forgot_password/', views.forgot_password, name='forgot_password'),
+    path('logout/', views.logout, name='logout'),
+    
     
     ]
--- website/landingpage/views.py
+++ website/landingpage/views.py
@@ -2,9 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+
 # Create your views here.
 def index(request):
-    return HttpResponse(render(request,'landingpage/home.html'))
+    return render(request,'landingpage/home.html')
 
 def login(request):
     return render(request, 'landingpage/login.html')
+
+def register(request):
+    return render(request, 'landingpage/register.html')
+
+def forgot_password(request):
+    return render(request, 'landingpage/forgot_password.html')
+
+def logout(request):
+    return render(request, 'landingpage/home.html')
+
+# def (request):
+#     return render(request, 'landingpage/.html')
